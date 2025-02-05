c99d1873c0bc2e3c2533abc11505d471a2288a71
Wenjiu1001 <1060967671@qq.com>
2025-02-05 09:35:45 +0800

    Update example.js

--- startup_scripts/example.js
+++ startup_scripts/example.js
@@ -1,30 +1,155 @@
-/*
-StartupEvents.registry('item', e => {
-    //物品轮子-多图层
-    e.create('soda_incan')
-        .color((itemstack, tintIndex) => global.setTint_one(itemstack, tintIndex))
-        .useAnimation('drink')
-        .useDuration(itemstack => 32)
-        .use((level, player, hand) => true)
-        .finishUsing((itemstack, level, entity) => {
-            if (itemstack.getOrCreateTag().potions !== undefined) {
-                itemstack.nbt.potions.forEach(ep => {
-                    entity.potionEffects.add(ep.effect, ep.time * 20, ep.level - 1, false, true)
-                })
-            }
-            if (entity.player) {
-                /** @type {$Player_}
-                let player = entity
-                level.server.schedule(1, (_) => { player.addItem(Item.of('kubejs:soda_can').item) })
-            }
-        })
-        .textureJson({
-            layer0: 'kubejs:item/food/soda/soda_incan_0',
-            layer1: 'kubejs:item/food/soda/soda_incan_1'
-        })
-        .tag("create:upright_on_belt")
-        .displayName("罐装汽水")
+let $AttributeModifier = Java.loadClass('net.minecraft.world.entity.ai.attributes.AttributeModifier');
+let $ItemAttributeModifierEvent = Java.loadClass('net.minecraftforge.event.ItemAttributeModifierEvent')
+let $UUid = Java.loadClass('java.util.UUID')
+let $EquipmentSlotType = Java.loadClass('net.minecraft.world.entity.EquipmentSlot')
 
-    return itemstack.shrink(1)
-})
-*/
+/**
+ * @type {Record<string,{attribute:Special.Attribute,value:number,operation:$AttributeModifier$Operation_}[]>}
+ */
+
+let customAttribute = {
+    'strength': [
+        { attribute: "minecraft:generic.attack_damage", value: 2, operation: "addition" }
+    ],
+    'speed': [
+        { attribute: "minecraft:generic.attack_speed", value: 2, operation: "multiply_base" },
+        { attribute: "minecraft:generic.attack_damage", value: 1, operation: "addition" }
+    ]
+};
+
+/**
+ * @type {Record<string,{attribute:Special.Attribute,value:number,operation:$AttributeModifier$Operation_}[]>}
+ */
+
+let customAttribute_armor = {
+    'durability': [
+        { attribute: "minecraft:generic.max_health", value: 10, operation: "addition" }
+    ],
+    'defense': [
+        { attribute: "minecraft:generic.armor", value: 2, operation: "addition" }
+    ]
+};
+
+ForgeEvents.onEvent('net.minecraftforge.event.ItemAttributeModifierEvent', (event) => {
+    let itemStack = event.getItemStack();
+    let slotType = event.getSlotType();
+
+    let nbt = itemStack.nbt;
+    if (nbt != null && nbt.mryh_attribute != undefined) {
+        let custom = customAttribute[nbt.mryh_attribute];
+        if (custom != undefined) {
+            console.log('测试')
+            if (slotType == $EquipmentSlotType.MAINHAND) {
+                custom.forEach((attr => {
+                    event.addModifier(attr.attribute,
+                        new $AttributeModifier(
+                            $UUid.randomUUID(),
+                            'customAttribute',
+                            attr.value,
+                            attr.operation
+                        ));
+                }))
+            }
+        } else {
+            console.error(`未找到该自定义属性${nbt.mryh_attribute}`);
+        }
+    }
+    if (nbt != null && nbt.mryh_attribute_armor_feet != undefined) {
+        let custom_armor_feet = customAttribute_armor[nbt.mryh_attribute_armor_feet];
+        if (custom_armor_feet != undefined) {
+            console.log('测试')
+            if (slotType == $EquipmentSlotType.FEET) {
+                custom_armor_feet.forEach((attr => {
+                    event.addModifier(attr.attribute,
+                        new $AttributeModifier(
+                            $UUid.randomUUID(),
+                            'customAttribute_armor',
+                            attr.value,
+                            attr.operation
+                        ));
+                }))
+            }
+        } else {
+            console.error(`未找到该自定义属性${nbt.mryh_attribute_armor_feet}`);
+        }
+    }
+    if (nbt != null && nbt.mryh_attribute_armor_chest != undefined) {
+        let custom_armor_chest = customAttribute_armor[nbt.mryh_attribute_armor_chest];
+        if (custom_armor_chest != undefined) {
+            console.log('测试')
+            if (slotType == $EquipmentSlotType.CHEST) {
+                custom_armor_chest.forEach((attr => {
+                    event.addModifier(attr.attribute,
+                        new $AttributeModifier(
+                            $UUid.randomUUID(),
+                            'customAttribute_armor',
+                            attr.value,
+                            attr.operation
+                        ));
+                }))
+            }
+        } else {
+            console.error(`未找到该自定义属性${nbt.mryh_attribute_armor_chest}`);
+        }
+    }
+    if (nbt != null && nbt.mryh_attribute_armor_legs != undefined) {
+        let custom_armor_legs = customAttribute_armor[nbt.mryh_attribute_armor_legs];
+        if (custom_armor_legs != undefined) {
+            console.log('测试')
+            if (slotType == $EquipmentSlotType.LEGS) {
+                custom_armor_legs.forEach((attr => {
+                    event.addModifier(attr.attribute,
+                        new $AttributeModifier(
+                            $UUid.randomUUID(),
+                            'customAttribute_armor',
+                            attr.value   价值,
+                            attr.operation   操作
+                        ));
+                }))
+            }
+        } else   其他 {
+            console.error(`未找到该自定义属性${nbt.mryh_attribute_armor_legs}`);
+        }
+    }
+    if   如果 (nbt != null && nbt.mryh_attribute_armor_head != undefined) {
+        let   让 custom_armor_head = customAttribute_armor[nbt.mryh_attribute_armor_head];
+        if   如果 (custom_armor_head != undefined) {
+            console.log('测试')
+            if   如果 (slotType == $EquipmentSlotType.HEAD   头) {
+                custom_armor_head.forEach((attr => {
+                    event.addModifier(attr.attribute,
+                        new   新 $AttributeModifier(
+                            $UUid.randomUUID(),
+                            'customAttribute_armor',
+                            attr.value   价值,
+                            attr.operation   操作
+                        ));
+                }))
+            }
+        } else {
+            console.error(`未找到该自定义属性${nbt.mryh_attribute_armor_head}`);
+        }
+    }
+});
+
+
+
+/*
+else if (nbt != null && nbt.customAttribute_armor != undefined) {
+    console.log('测试')
+    if ([$EquipmentSlotType.CHEST, $EquipmentSlotType.HEAD, $EquipmentSlotType.LEGS, $EquipmentSlotType.FEET].includes(slotType)) {
+        let custom_armor = customAttribute_armor[nbt.customAttribute_armor];
+        if (custom_armor != undefined) {
+            custom_armor.forEach((attr => {
+                event.addModifier(attr.attribute,
+                    new $AttributeModifier(
+                        $UUid.randomUUID(),
+                        'customAttribute',
+                        attr.value,
+                        attr.operation
+                    ));
+            }));
+        }
+    }
+} 
+    */
