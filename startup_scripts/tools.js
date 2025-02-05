f7bfc08c45ff04bf646e9e59310dc03b9bd662ad
Wenjiu1001 <1060967671@qq.com>
2025-02-05 09:33:21 +0800

    Update tools.js

---
+++ startup_scripts/tools.js
@@ -0,0 +1,21 @@
+function   函数 tryCatch(fun, args) {
+    try {
+        // 尝试执行传入的函数fun，并传入参数args
+        fun(args)
+    } catch (error) {
+        // 如果在执行fun函数时发生错误，捕获错误并打印到控制台
+        console.error(error)
+    }
+}
+
+/**
+ * @param {$LivingEntityHurtEventJS_} event - 事件对象
+ */
+ForgeEvents.onEvent('net.minecraftforge.event.entity.living.LivingHurtEvent', event => {
+    if (event.source.player) {
+        global.HurtCoreByPlayer(event);
+    } else {
+        global.HurtCoreByOther(event);
+    }
+});
+
