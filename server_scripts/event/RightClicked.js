bfca54a1d60230cf7821090c6014092aef8517a1
Wenjiu1001 <1060967671@qq.com>
2025-02-17 11:02:27 +0800

    Update RightClicked.js

--- server_scripts/event/RightClicked.js
+++ server_scripts/event/RightClicked.js
@@ -1,12 +1,13 @@
 // 监听玩家右键点击物品的事件
 ItemEvents.rightClicked((event) => {
     // 获取被使用的物品ID
-    const itemID = event.item.id;
+    let itemID = event.item.id;
     // 设置禁止使用的物品ID列表
-    const forbiddenItems = [
+    let forbiddenItems = [
         'minecraft:milk_bucket',
         'minecraft:milk_bottle',
-        'ecologics:coconut_slice'
+        'ecologics:coconut_slice',
+        'farmrtsdelight:hot_cocoa'
     ];
     // 检查物品ID是否在禁止列表中
     if (forbiddenItems.includes(itemID)) {
@@ -18,9 +19,9 @@ ItemEvents.rightClicked((event) => {
 // 监听物品右键点击事件
 ItemEvents.firstRightClicked((event) => {
     // 获取被使用的物品ID
-    const itemID = event.item.id
+    let itemID = event.item.id
     // 设置使用的物品ID列表
-    const itemlist = [
+    let itemlist = [
       'touhou_little_maid:favorability_tool_add',
       "touhou_little_maid:favorability_tool_reduce"
     ]
@@ -31,3 +32,21 @@ ItemEvents.firstRightClicked((event) => {
     }
 });
 
+// 监听方块右键事件
+BlockEvents.rightClicked(event => {
+    // 获取玩家手中的物品ID和被右键的方块ID
+    let itemID = event.player.getItem()
+    let blockID = event.getBlock()
+
+    // 检查是否是打火石和黑曜石
+    if (itemID == 'minecraft:flint_and_steel' && blockID == 'minecraft:obsidian') {
+        // 获取玩家
+        let player = event.getPlayer();
+
+        // 如果玩家存在，则发送提示信息
+        if (player) {
+            // 发送聊天信息到玩家
+            player.tell('下界通道已被摧毁');
+        }
+    }
+});
