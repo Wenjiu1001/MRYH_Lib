59a34a642b3ccfdc7c5df548bc757b17db02c074
Wenjiu1001 <1060967671@qq.com>
2025-02-17 11:00:22 +0800

    Update zombie_extreme.js

--- server_scripts/entityloot/zombie_extreme.js
+++ server_scripts/entityloot/zombie_extreme.js
@@ -1,230 +1,230 @@
+
 ServerEvents.entityLootTables(event => {
     event.modifyEntity("zombie_extreme:crawler", Loot => {
         Loot.addPool(pool => {
             pool.killedByPlayer()
-            pool.addItem("copper_ingot").weight(300)
-            pool.addItem("coal").weight(200)
-            pool.addItem("iron_ingot").weight(150)
-            pool.addItem("gold_ingot").weight(100)
-            pool.addItem("diamond").weight(50)
-            pool.addItem("emerald").weight(10)
-            pool.addItem("netherite_scrap").weight(1)
-            pool.addEmpty(189)
+            pool.addItem("copper_ingot").weight(150) // 300 / 2
+            pool.addItem("coal").weight(100) // 200 / 2
+            pool.addItem("iron_ingot").weight(75) // 150 / 2
+            pool.addItem("gold_ingot").weight(50) // 100 / 2
+            pool.addItem("diamond").weight(25) // 50 / 2
+            pool.addItem("emerald").weight(10) // 不变
+            pool.addItem("netherite_scrap").weight(1) // 不变
+            pool.addEmpty(414) // 1000 - (150+100+75+50+25+10+1) = 414
         })
         Loot.addPool(pool => {
-            pool.addItem("apocalypsenow:money").weight(600).count([1, 2])
-            pool.addItem("apocalypsenow:money_block").weight(10)
-            pool.addEmpty(390)
+            pool.addItem("apocalypsenow:money").weight(300) // 600 / 2
+            pool.addItem("apocalypsenow:money_block").weight(5) // 10 / 2
+            pool.addEmpty(695) // 1000 - (300+5) = 695
         })
     })
 
     event.modifyEntity("zombie_extreme:devestated", Loot => {
         Loot.addPool(pool => {
             pool.killedByPlayer()
-            pool.addItem("copper_ingot").weight(300)
-            pool.addItem("coal").weight(200)
-            pool.addItem("iron_ingot").weight(150)
-            pool.addItem("gold_ingot").weight(100)
-            pool.addItem("diamond").weight(50)
-            pool.addItem("emerald").weight(10)
-            pool.addItem("netherite_scrap").weight(1)
-            pool.addEmpty(189)
+            pool.addItem("copper_ingot").weight(150) // 300 / 2
+            pool.addItem("coal").weight(100) // 200 / 2
+            pool.addItem("iron_ingot").weight(75) // 150 / 2
+            pool.addItem("gold_ingot").weight(50) // 100 / 2
+            pool.addItem("diamond").weight(25) // 50 / 2
+            pool.addItem("emerald").weight(10) // 不变
+            pool.addItem("netherite_scrap").weight(1) // 不变
+            pool.addEmpty(414) // 1000 - (150+100+75+50+25+10+1) = 414
         })
         Loot.addPool(pool => {
-            pool.addItem("apocalypsenow:money").weight(600).count([1, 2])
-            pool.addItem("apocalypsenow:money_block").weight(10)
-            pool.addEmpty(390)
+            pool.addItem("apocalypsenow:money").weight(300) // 600 / 2
+            pool.addItem("apocalypsenow:money_block").weight(5) // 10 / 2
+            pool.addEmpty(695) // 1000 - (300+5) = 695
         })
     })
 
     event.modifyEntity("zombie_extreme:divided", Loot => {
         Loot.addPool(pool => {
             pool.killedByPlayer()
-            pool.addItem("copper_ingot").weight(300)
-            pool.addItem("coal").weight(200)
-            pool.addItem("iron_ingot").weight(150)
-            pool.addItem("gold_ingot").weight(100)
-            pool.addItem("diamond").weight(50)
-            pool.addItem("emerald").weight(10)
-            pool.addItem("netherite_scrap").weight(1)
-            pool.addEmpty(189)
+            pool.addItem("copper_ingot").weight(150) // 300 / 2
+            pool.addItem("coal").weight(100) // 200 / 2
+            pool.addItem("iron_ingot").weight(75) // 150 / 2
+            pool.addItem("gold_ingot").weight(50) // 100 / 2
+            pool.addItem("diamond").weight(25) // 50 / 2
+            pool.addItem("emerald").weight(10) // 不变
+            pool.addItem("netherite_scrap").weight(1) // 不变
+            pool.addEmpty(414) // 1000 - (150+100+75+50+25+10+1) = 414
         })
         Loot.addPool(pool => {
-            pool.addItem("apocalypsenow:money").weight(600).count([1, 2])
-            pool.addItem("apocalypsenow:money_block").weight(10)
-            pool.addEmpty(390)
+            pool.addItem("apocalypsenow:money").weight(300) // 600 / 2
+            pool.addItem("apocalypsenow:money_block").weight(5) // 10 / 2
+            pool.addEmpty(695) // 1000 - (300+5) = 695
         })
     })
 
     event.modifyEntity("zombie_extreme:explosive_infected", Loot => {
         Loot.addPool(pool => {
             pool.killedByPlayer()
-            pool.addItem("copper_ingot").weight(300)
-            pool.addItem("coal").weight(200)
-            pool.addItem("iron_ingot").weight(150)
-            pool.addItem("gold_ingot").weight(100)
-            pool.addItem("diamond").weight(50)
-            pool.addItem("emerald").weight(10)
-            pool.addItem("netherite_scrap").weight(1)
-            pool.addEmpty(189)
+            pool.addItem("copper_ingot").weight(150) // 300 / 2
+            pool.addItem("coal").weight(100) // 200 / 2
+            pool.addItem("iron_ingot").weight(75) // 150 / 2
+            pool.addItem("gold_ingot").weight(50) // 100 / 2
+            pool.addItem("diamond").weight(25) // 50 / 2
+            pool.addItem("emerald").weight(10) // 不变
+            pool.addItem("netherite_scrap").weight(1) // 不变
+            pool.addEmpty(414) // 1000 - (150+100+75+50+25+10+1) = 414
         })
         Loot.addPool(pool => {
-            pool.addItem("apocalypsenow:money").weight(600).count([1, 2])
-            pool.addItem("apocalypsenow:money_block").weight(10)
-            pool.addEmpty(390)
+            pool.addItem("apocalypsenow:money").weight(300) // 600 / 2
+            pool.addItem("apocalypsenow:money_block").weight(5) // 10 / 2
+            pool.addEmpty(695) // 1000 - (300+5) = 695
         })
     })
 
     event.modifyEntity("zombie_extreme:infected", Loot => {
         Loot.addPool(pool => {
             pool.killedByPlayer()
-            pool.addItem("copper_ingot").weight(300)
-            pool.addItem("coal").weight(200)
-            pool.addItem("iron_ingot").weight(150)
-            pool.addItem("gold_ingot").weight(100)
-            pool.addItem("diamond").weight(50)
-            pool.addItem("emerald").weight(10)
-            pool.addItem("netherite_scrap").weight(1)
-            pool.addEmpty(189)
+            pool.addItem("copper_ingot").weight(150) // 300 / 2
+            pool.addItem("coal").weight(100) // 200 / 2
+            pool.addItem("iron_ingot").weight(75) // 150 / 2
+            pool.addItem("gold_ingot").weight(50) // 100 / 2
+            pool.addItem("diamond").weight(25) // 50 / 2
+            pool.addItem("emerald").weight(10) // 不变
+            pool.addItem("netherite_scrap").weight(1) // 不变
+            pool.addEmpty(414) // 1000 - (150+100+75+50+25+10+1) = 414
         })
         Loot.addPool(pool => {
-            pool.addItem("apocalypsenow:money").weight(600).count([1, 2])
-            pool.addItem("apocalypsenow:money_block").weight(10)
-            pool.addEmpty(390)
+            pool.addItem("apocalypsenow:money").weight(300) // 600 / 2
+            pool.addItem("apocalypsenow:money_block").weight(5) // 10 / 2
+            pool.addEmpty(695) // 1000 - (300+5) = 695
         })
     })
 
     event.modifyEntity("zombie_extreme:infected_hazmat", Loot => {
         Loot.addPool(pool => {
             pool.killedByPlayer()
-            pool.addItem("copper_ingot").weight(300)
-            pool.addItem("coal").weight(200)
-            pool.addItem("iron_ingot").weight(150)
-            pool.addItem("gold_ingot").weight(100)
-            pool.addItem("diamond").weight(50)
-            pool.addItem("emerald").weight(10)
-            pool.addItem("netherite_scrap").weight(1)
-            pool.addEmpty(189)
+            pool.addItem("copper_ingot").weight(150) // 300 / 2
+            pool.addItem("coal").weight(100) // 200 / 2
+            pool.addItem("iron_ingot").weight(75) // 150 / 2
+            pool.addItem("gold_ingot").weight(50) // 100 / 2
+            pool.addItem("diamond").weight(25) // 50 / 2
+            pool.addItem("emerald").weight(10) // 不变
+            pool.addItem("netherite_scrap").weight(1) // 不变
+            pool.addEmpty(414) // 1000 - (150+100+75+50+25+10+1) = 414
         })
         Loot.addPool(pool => {
-            pool.addItem("apocalypsenow:money").weight(600).count([1, 2])
-            pool.addItem("apocalypsenow:money_block").weight(10)
-            pool.addEmpty(390)
+            pool.addItem("apocalypsenow:money").weight(300) // 600 / 2
+            pool.addItem("apocalypsenow:money_block").weight(5) // 10 / 2
+            pool.addEmpty(695) // 1000 - (300+5) = 695
         })
     })
 
     event.modifyEntity("zombie_extreme:infected_military", Loot => {
         Loot.addPool(pool => {
             pool.killedByPlayer()
-            pool.addItem("copper_ingot").weight(300)
-            pool.addItem("coal").weight(200)
-            pool.addItem("iron_ingot").weight(150)
-            pool.addItem("gold_ingot").weight(100)
-            pool.addItem("diamond").weight(50)
-            pool.addItem("emerald").weight(10)
-            pool.addItem("netherite_scrap").weight(1)
-            pool.addEmpty(189)
+            pool.addItem("copper_ingot").weight(150) // 300 / 2
+            pool.addItem("coal").weight(100) // 200 / 2
+            pool.addItem("iron_ingot").weight(75) // 150 / 2
+            pool.addItem("gold_ingot").weight(50) // 100 / 2
+            pool.addItem("diamond").weight(25) // 50 / 2
+            pool.addItem("emerald").weight(10) // 不变
+            pool.addItem("netherite_scrap").weight(1) // 不变
+            pool.addEmpty(414) // 1000 - (150+100+75+50+25+10+1) = 414
         })
         Loot.addPool(pool => {
-            pool.addItem("apocalypsenow:money").weight(600).count([1, 2])
-            pool.addItem("apocalypsenow:money_block").weight(10)
-            pool.addEmpty(390)
+            pool.addItem("apocalypsenow:money").weight(300) // 600 / 2
+            pool.addItem("apocalypsenow:money_block").weight(5) // 10 / 2
+            pool.addEmpty(695) // 1000 - (300+5) = 695
         })
     })
 
     event.modifyEntity("zombie_extreme:infected_police", Loot => {
         Loot.addPool(pool => {
             pool.killedByPlayer()
-            pool.addItem("copper_ingot").weight(300)
-            pool.addItem("coal").weight(200)
-            pool.addItem("iron_ingot").weight(150)
-            pool.addItem("gold_ingot").weight(100)
-            pool.addItem("diamond").weight(50)
-            pool.addItem("emerald").weight(10)
-            pool.addItem("netherite_scrap").weight(1)
-            pool.addEmpty(189)
+            pool.addItem("copper_ingot").weight(150) // 300 / 2
+            pool.addItem("coal").weight(100) // 200 / 2
+            pool.addItem("iron_ingot").weight(75) // 150 / 2
+            pool.addItem("gold_ingot").weight(50) // 100 / 2
+            pool.addItem("diamond").weight(25) // 50 / 2
+            pool.addItem("emerald").weight(10) // 不变
+            pool.addItem("netherite_scrap").weight(1) // 不变
+            pool.addEmpty(414) // 1000 - (150+100+75+50+25+10+1) = 414
         })
         Loot.addPool(pool => {
-            pool.addItem("apocalypsenow:money").weight(600).count([1, 2])
-            pool.addItem("apocalypsenow:money_block").weight(10)
-            pool.addEmpty(390)
+            pool.addItem("apocalypsenow:money").weight(300) // 600 / 2
+            pool.addItem("apocalypsenow:money_block").weight(5) // 10 / 2
+            pool.addEmpty(695) // 1000 - (300+5) = 695
         })
     })
 
     event.modifyEntity("zombie_extreme:inflated", Loot => {
         Loot.addPool(pool => {
             pool.killedByPlayer()
-            pool.addItem("copper_ingot").weight(300)
-            pool.addItem("coal").weight(200)
-            pool.addItem("iron_ingot").weight(150)
-            pool.addItem("gold_ingot").weight(100)
-            pool.addItem("diamond").weight(50)
-            pool.addItem("emerald").weight(10)
-            pool.addItem("netherite_scrap").weight(1)
-            pool.addEmpty(189)
+            pool.addItem("copper_ingot").weight(150) // 300 / 2
+            pool.addItem("coal").weight(100) // 200 / 2
+            pool.addItem("iron_ingot").weight(75) // 150 / 2
+            pool.addItem("gold_ingot").weight(50) // 100 / 2
+            pool.addItem("diamond").weight(25) // 50 / 2
+            pool.addItem("emerald").weight(10) // 不变
+            pool.addItem("netherite_scrap").weight(1) // 不变
+            pool.addEmpty(414) // 1000 - (150+100+75+50+25+10+1) = 414
         })
         Loot.addPool(pool => {
-            pool.addItem("apocalypsenow:money").weight(600).count([1, 2])
-            pool.addItem("apocalypsenow:money_block").weight(10)
-            pool.addEmpty(390)
+            pool.addItem("apocalypsenow:money").weight(300) // 600 / 2
+            pool.addItem("apocalypsenow:money_block").weight(5) // 10 / 2
+            pool.addEmpty(695) // 1000 - (300+5) = 695
         })
     })
 
     event.modifyEntity("zombie_extreme:military", Loot => {
         Loot.addPool(pool => {
             pool.killedByPlayer()
-            pool.addItem("copper_ingot").weight(300)
-            pool.addItem("coal").weight(200)
-            pool.addItem("iron_ingot").weight(150)
-            pool.addItem("gold_ingot").weight(100)
-            pool.addItem("diamond").weight(50)
-            pool.addItem("emerald").weight(10)
-            pool.addItem("netherite_scrap").weight(1)
-            pool.addEmpty(189)
+            pool.addItem("copper_ingot").weight(150) // 300 / 2
+            pool.addItem("coal").weight(100) // 200 / 2
+            pool.addItem("iron_ingot").weight(75) // 150 / 2
+            pool.addItem("gold_ingot").weight(50) // 100 / 2
+            pool.addItem("diamond").weight(25) // 50 / 2
+            pool.addItem("emerald").weight(10) // 不变
+            pool.addItem("netherite_scrap").weight(1) // 不变
+            pool.addEmpty(414) // 1000 - (150+100+75+50+25+10+1) = 414
         })
         Loot.addPool(pool => {
-            pool.addItem("apocalypsenow:money").weight(600).count([1, 2])
-            pool.addItem("apocalypsenow:money_block").weight(10)
-            pool.addEmpty(390)
+            pool.addItem("apocalypsenow:money").weight(300) // 600 / 2
+            pool.addItem("apocalypsenow:money_block").weight(5) // 10 / 2
+            pool.addEmpty(695) // 1000 - (300+5) = 695
         })
     })
 
     event.modifyEntity("zombie_extreme:parasite", Loot => {
         Loot.addPool(pool => {
             pool.killedByPlayer()
-            pool.addItem("copper_ingot").weight(300)
-            pool.addItem("coal").weight(200)
-            pool.addItem("iron_ingot").weight(150)
-            pool.addItem("gold_ingot").weight(100)
-            pool.addItem("diamond").weight(50)
-            pool.addItem("emerald").weight(10)
-            pool.addItem("netherite_scrap").weight(1)
-            pool.addEmpty(189)
+            pool.addItem("copper_ingot").weight(150) // 300 / 2
+            pool.addItem("coal").weight(100) // 200 / 2
+            pool.addItem("iron_ingot").weight(75) // 150 / 2
+            pool.addItem("gold_ingot").weight(50) // 100 / 2
+            pool.addItem("diamond").weight(25) // 50 / 2
+            pool.addItem("emerald").weight(10) // 不变
+            pool.addItem("netherite_scrap").weight(1) // 不变
+            pool.addEmpty(414) // 1000 - (150+100+75+50+25+10+1) = 414
         })
         Loot.addPool(pool => {
-            pool.addItem("apocalypsenow:money").weight(600).count([1, 2])
-            pool.addItem("apocalypsenow:money_block").weight(10)
-            pool.addEmpty(390)
+            pool.addItem("apocalypsenow:money").weight(300) // 600 / 2
+            pool.addItem("apocalypsenow:money_block").weight(5) // 10 / 2
+            pool.addEmpty(695) // 1000 - (300+5) = 695
         })
     })
 
     event.modifyEntity("zombie_extreme:runner", Loot => {
         Loot.addPool(pool => {
             pool.killedByPlayer()
-            pool.addItem("copper_ingot").weight(300)
-            pool.addItem("coal").weight(200)
-            pool.addItem("iron_ingot").weight(150)
-            pool.addItem("gold_ingot").weight(100)
-            pool.addItem("diamond").weight(50)
-            pool.addItem("emerald").weight(10)
-            pool.addItem("netherite_scrap").weight(1)
-            pool.addEmpty(189)
+            pool.addItem("copper_ingot").weight(150) // 300 / 2
+            pool.addItem("coal").weight(100) // 200 / 2
+            pool.addItem("iron_ingot").weight(75) // 150 / 2
+            pool.addItem("gold_ingot").weight(50) // 100 / 2
+            pool.addItem("diamond").weight(25) // 50 / 2
+            pool.addItem("emerald").weight(10) // 不变
+            pool.addItem("netherite_scrap").weight(1) // 不变
+            pool.addEmpty(414) // 1000 - (150+100+75+50+25+10+1) = 414
         })
         Loot.addPool(pool => {
-            pool.addItem("apocalypsenow:money").weight(600).count([1, 2])
-            pool.addItem("apocalypsenow:money_block").weight(10)
-            pool.addEmpty(390)
+            pool.addItem("apocalypsenow:money").weight(300) // 600 / 2
+            pool.addItem("apocalypsenow:money_block").weight(5) // 10 / 2
+            pool.addEmpty(695) // 1000 - (300+5) = 695
         })
     })
-
 })
