c592d5bb2089a30c0fd0af5028f3da2275bac143
Wenjiu1001 <1060967671@qq.com>
2025-02-17 11:14:12 +0800

    Delete startup_scripts/music.js

--- startup_scripts/music.js
+++
@@ -1,9 +0,0 @@
-StartupEvents.registry("sound_event",event=>{
-    event.create("jiuhu:music.ciallo")
-})
-
-StartupEvents.registry("item",event=>{
-    event.create("jiuhu:ciallo","music_disc")
-            .song("jiuhu:music.ciallo",237)
-            .tag("music_discs")
-})
