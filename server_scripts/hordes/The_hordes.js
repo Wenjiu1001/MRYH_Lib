62cd26aeaddb4cccb8f8aad27c9794d52167105c
Wenjiu1001 <1060967671@qq.com>
2025-04-06 10:51:18 +0800

    Update The_hordes.js

--- server_scripts/hordes/The_hordes.js
+++ server_scripts/hordes/The_hordes.js
@@ -1,14 +1,14 @@
 
 // 会引起尸潮的特殊物品（例：CDU）
-const specialItems = [
+let specialItems = [
     "spore:cdu"
     // 可以在这里添加更多特殊物品
 ];
 
 // 标记每个特殊物品是否已经触发过尸潮
-const blockMarks = new Array(specialItems.length).fill(false);
+let blockMarks = new Array(specialItems.length).fill(false);
 // 用注册到访结构进度来代替访问结构
-const specialStructures = [
+let specialStructures = [
     "dungeons_arise:find_abandoned_temple",
     "dungeons_arise:find.aviary",
     "dungeons_arise:find_bandit_towers",
@@ -77,29 +77,35 @@ function updateHordeType(day) {
     }
 }
 
+let horde_spawn=0
+
 LevelEvents.tick(event => {
-    const day = Math.floor(event.level.dayTime() / 24000);
-    const dayTick = event.level.dayTime() % 24000;
+    let day = Math.floor(event.level.dayTime() / 24000);
+    let dayTick = event.level.dayTime() % 24000;
 
     // 夜晚时间从13000刻到23990刻
-    const isNightTime = dayTick >= 13000 && dayTick <= 23990;
+    let isNightTime = dayTick >= 13000 && dayTick <= 23990;
 
     // 每五天触发一次尸潮
-    if (isNightTime && day % 5 === 0) {
-        const hordeType = updateHordeType(day);
+    if (isNightTime && day % 5 == 0 && day !== 0) {
+        let hordeType = updateHordeType(day);
 
         // 直接触发尸潮
         if (dayTick === 13000) {
-            event.server.runCommandSilent(`/hordes spawnWave @r 40 ${hordeType}`);
+            event.server.runCommandSilent(`/hordes spawnWave @r 30 ${hordeType}`);
+            /*
+            event.server.runCommandSilent(`/playsound mryh:music.horde_spawn player @a ~ ~ ~`);
+            */
+           event.server.sendData('mryh:hordes_spawn',{ hordes_spawn: 1 })
         }
         if (dayTick === 16000) {
-            event.server.runCommandSilent(`/hordes spawnWave @r 50 ${hordeType}`);
+            event.server.runCommandSilent(`/hordes spawnWave @r 40 ${hordeType}`);
         }
         if (dayTick === 19000) {
-            event.server.runCommandSilent(`/hordes spawnWave @r 60 ${hordeType}`);
+            event.server.runCommandSilent(`/hordes spawnWave @r 50 ${hordeType}`);
         }
         if (dayTick === 22000) {
-            event.server.runCommandSilent(`/hordes spawnWave @r 70 ${hordeType}`);
+            event.server.runCommandSilent(`/hordes spawnWave @r 60 ${hordeType}`);
         }
     }
 
@@ -108,8 +114,8 @@ LevelEvents.tick(event => {
         for (let i = 0; i < 13000; i++) { // 总共执行13000次
             if (Math.random() < 0.1) { // 以20%的概率执行
                 a += 1; // 增加a的值
-                if (a === 10000) { // 如果a达到10000
-                    const hordeType = updateHordeType(day);
+                if (a == 15000) { // 如果a达到10000
+                    let hordeType = updateHordeType(day);
                     event.server.runCommandSilent(`/hordes spawnWave @r 50 ${hordeType}`); // 执行命令
                     a = 0; // 重置a的值
                     event.server.tell('很不幸！随机尸潮爆发了！')
@@ -121,13 +127,13 @@ LevelEvents.tick(event => {
 
 
 BlockEvents.broken(event => {
-    const day = Math.floor(event.level.dayTime() / 24000);
-    const item = event.block.id;
-    const hordeType = updateHordeType(day);
-    const player = event.player.name.string;
+    let day = Math.floor(event.level.dayTime() / 24000);
+    let item = event.block.id;
+    let hordeType = updateHordeType(day);
+    let player = event.player.name.string;
 
     // 检查被破坏的物品是否在特殊物品数组中，并且游戏天数大于10
-    const itemIndex = specialItems.indexOf(item);
+    let itemIndex = specialItems.indexOf(item);
     if (itemIndex !== -1 && day > 10 && !blockMarks[itemIndex]) {
         event.server.runCommandSilent(`/hordes spawnWave ${player} 50 ${hordeType}`);
         event.server.runCommandSilent(`/title ${player} title {"text":"你惊动了尸潮！","color":"red","bold":true}`);
@@ -137,10 +143,10 @@ BlockEvents.broken(event => {
 
 
 PlayerEvents.advancement(event => {
-    const day = Math.floor(event.level.dayTime() / 24000);
-    const Structure = event.advancement
-    const player = event.player.name.string;
-    const hordeType = updateHordeType(day);
+    let day = Math.floor(event.level.dayTime() / 24000);
+    let Structure = event.advancement
+    let player = event.player.name.string;
+    let hordeType = updateHordeType(day);
     if (specialStructures.includes(Structure.id().toString()) && day > 10) {
         event.server.runCommandSilent(`/hordes spawnWave ${player} 30 ${hordeType}`);
     }
