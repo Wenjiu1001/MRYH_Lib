db872163a7512f57d0303404b859019b28c4fde2
Wenjiu1001 <1060967671@qq.com>
2025-04-06 10:54:36 +0800

    Update music.js

--- startup_scripts/music.js
+++ startup_scripts/music.js
@@ -1,9 +1,10 @@
-StartupEvents.registry("sound_event",event=>{
+StartupEvents.registry("sound_event", event => {
     event.create("jiuhu:music.ciallo")
+    event.create("mryh:music.horde_spawn")
 })
 
-StartupEvents.registry("item",event=>{
-    event.create("jiuhu:ciallo","music_disc")
-            .song("jiuhu:music.ciallo",237)
-            .tag("music_discs")
+StartupEvents.registry("item", event => {
+    event.create("jiuhu:ciallo", "music_disc")
+        .song("jiuhu:music.ciallo", 237)
+        .tag("music_discs")
 })
