0d75206b8bc352f76946fb1d2f084897b8c49d06
Wenjiu1001 <1060967671@qq.com>
2025-04-06 10:47:03 +0800

    Update debug.js

--- server_scripts/event/debug.js
+++ server_scripts/event/debug.js
@@ -1,46 +1,36 @@
 
-/*ServerEvents.blockLootTables((event) => {
-    event.modifyBlock('apocalypsenow:blocks/dropboxcommon', (loot) => {
-        // 遍历战利品表中的所有奖池
-        loot.pools.forEach((pool) => {
-            // 遍历奖池中的所有物品
-            pool.entries.forEach((entry) => {
-                // 输出物品ID
-                console.log(entry.name);
-            });
-        });
-    });
-});*/
-
-EntityEvents.hurt(event => {
-    let enchantlevel = event.source.player.mainHandItem.getEnchantmentLevel("mryh:slowness_bullet")
-    if (event.source.actual.isPlayer()) {
-        if (event.source.player.mainHandItem.getEnchantmentLevel("mryh:slowness_bullet")) {
-            event.entity.potionEffects.add('minecraft:slowness', 30, enchantlevel)
-        }
-    }
-});
-
 
 /**
+ * @param {Internal.LivingHurtEvent_} event 
  * @param {$LivingEntityHurtEventJS_} event
  * @returns
  */
-
 // 处理非玩家造成的伤害
 global.HurtCoreByOther = event => {
-    // 这里添加处理非玩家造成的伤害的逻辑
-    console.log("Handling damage by other", event);
-};
-
-global.HurtCoreByPlayer = event => {
-    // 设置被攻击者的无敌帧为1帧
+    /*
     event.entity.invulnerableTime = 10 + 1;
-    let Damage = event.getSource().getType().toString();
-    // 不同的伤害类型会转向不同的函数，优先根据数值计算出初步伤害
-    handleDamageByPlayer(event);
+    /*
+    TermPlayerHurtByOthers(event)
+    */
 };
 
+/** 
+ * @param {Internal.LivingHurtEvent_} event 
+ */
+global.HurtCoreByPlayer = event => {
+    event.entity.invulnerableTime = 10 + 1;
+    /*
+    let Damage = event.getSource().getType().toString();
+    */
+    handleDamageByPlayer(event);
+    /*
+    TermEntityHurtByPlayers(event)
+    */
+};
+
+/** 
+ * @param {Internal.LivingHurtEvent_} event 
+ */
 function handleDamageByPlayer(event) {
     let DamageType = event.getSource().getType().toString();
     switch (DamageType) {
@@ -51,10 +41,22 @@ function handleDamageByPlayer(event) {
     }
 }
 
+/** 
+ * @param {Internal.LivingHurtEvent_} event 
+ */
 function BulletDamageByPlayer(event) {
-    let enchantlevel = event.source.player.mainHandItem.getEnchantmentLevel("mryh:killeradd_bullet")
-    let count = event.amount * (1 + 0.5 * enchantlevel)
-    if (enchantlevel) {
+    let enchantlevel = event.source.player.mainHandItem
+    // 战术射击增强
+    if (enchantlevel.getEnchantmentLevel("mryh:killeradd_bullet")) {
+        let count = event.amount * (1 + 0.1 * enchantlevel.getEnchantmentLevel("mryh:killeradd_bullet"))
         event.setAmount(count)
+    }
+    // 减速弹
+    if (enchantlevel.getEnchantmentLevel("mryh:slowness_bullet")) {
+        /**
+         * @type {Internal.LivingEntity}
+         */
+        let entity = event.entity
+        entity.potionEffects.add('minecraft:slowness', 60, enchantlevel.getEnchantmentLevel("mryh:slowness_bullet"))
     }
 }
