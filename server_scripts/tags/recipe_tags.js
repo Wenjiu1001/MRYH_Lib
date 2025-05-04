73340b6bf64c0b5e086525e8a0e806b7170db33e
Wenjiu1001 <1060967671@qq.com>
2025-05-04 19:43:48 +0800

    Add files via upload

---
+++ server_scripts/tags/recipe_tags.js
@@ -0,0 +1,6 @@
+//标签修改
+ServerEvents.tags('item',event =>{
+    event.add('forge:rubber','marbledsarsenal:rubber')
+    event.add('forge:rubber','marbledsapi:rubber')
+    event.add('forge:rubber','zombie_extreme:rubber')
+})
