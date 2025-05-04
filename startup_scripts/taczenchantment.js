09fa476ce9818284caaf045d00e458af89f1e8c6
Wenjiu1001 <1060967671@qq.com>
2025-05-04 19:45:30 +0800

    Update taczenchantment.js

--- startup_scripts/taczenchantment.js
+++ startup_scripts/taczenchantment.js
@@ -1,25 +1,60 @@
 
-
 let $EnchantmentCategory = Java.loadClass('net.minecraft.world.item.enchantment.EnchantmentCategory')
+let $ModernKineticGunItem = Java.loadClass('com.tacz.guns.item.ModernKineticGunItem')
+let $TimelessAPI = Java.loadClass("com.tacz.guns.api.TimelessAPI")
+let $Integer = Java.loadClass('java.lang.Integer')
 
-$EnchantmentCategory.create('test', (i) => true)
+// tacz的枪械通用附魔类别
+let tacz_common = $EnchantmentCategory.create('TACZC', (item) => item instanceof $ModernKineticGunItem)
+let un = $EnchantmentCategory.create('UN', (item) => true)
+
 StartupEvents.registry("enchantment", e => {
-    e.create("mryh:slowness_bullet")
-        .canEnchant((item) => {
-            return item.id == 'tacz:modern_kinetic_gun'
-        })
+
+    e.create("mryh:tactical_enhancement")
+        .category(tacz_common)
         .minLevel(1)
         .maxLevel(5)
-        .category('test')
-        .rarity('very_rare')
-        .displayName("减速子弹")
-    e.create("mryh:killeradd_bullet")
-        .canEnchant((item) => {
-            return item.id == 'tacz:modern_kinetic_gun'
-        })
-        .minLevel(1)
-        .maxLevel(5)
-        .category('test')
-        .rarity('very_rare')
+        .veryRare()
         .displayName("增强射击战术")
+
+    e.create("mryh:slowing_bullets")
+        .category(tacz_common)
+        .minLevel(1)
+        .maxLevel(5)
+        .veryRare()
+        .displayName("减速子弹")
+    e.create("mryh:unbreakable")
+        .category(un)
+        .displayName("无法破坏")
+})
+
+
+ItemEvents.modification(e => {
+
+    e.modify('tacz:modern_kinetic_gun', item => {
+
+        // 忆然：给枪械附魔能力
+        item.setEnchantmentValueFunction((stack) => {
+
+            let value
+
+            $TimelessAPI.getCommonGunIndex(item.getGunId(stack)).ifPresent(index => {
+
+                // 筛选枪械类型
+                switch (index.getType()) {
+                    // 霰弹枪
+                    case "shotgun":
+                        return value = 10
+                    // 未指定类型
+                    default:
+                        return value = 1
+                }
+
+            })
+
+            return $Integer.valueOf(String(value))
+
+        })
+    })
+
 })
