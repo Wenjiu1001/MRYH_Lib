a9e2854f1f19d83a165db200bfda8fb4f820e9e8
Wenjiu1001 <1060967671@qq.com>
2025-05-04 19:26:09 +0800

    Update tooltips_mryh.js

--- client_scripts/tooltips_mryh.js
+++ client_scripts/tooltips_mryh.js
@@ -2,38 +2,38 @@
 
 
 
-ItemEvents.tooltip(event   事件 => {
-    let   让 lang = "kubejs/assets/mryh/lang/" + Client.languageManager.selected.toString() + ".json";
-    let   让 json = JsonIO.read(lang);
+ItemEvents.tooltip(event => {
+    let lang = "kubejs/assets/mryh/lang/" + Client.languageManager.selected.toString() + ".json";
+    let json = JsonIO.read(lang);
     json = JsonIO.toObject(json);
 
     json.forEach(element => {
         // 根据langkey组织物品id
-        if   如果 (element.includes("item.") && element.includes("tooltip")) {
+        if (element.includes("item.") && element.includes("tooltip")) {
             // 获取modid
-            let   让 modid = element.split   分裂('.')[1];
+            let modid = element.split('.')[1];
             // 将id和langkey组织到1个列表，以便于addAdvanced的内部函数可以访问到langkey
-            let   让 itemID = modid + ":" + element.split   分裂('.')[2];
-            let   让 itemElement = [itemID, element];
-            event   事件.addAdvanced(itemElement[0], (item, advance, text) => {
+            let itemID = modid + ":" + element.split('.')[2];
+            let itemElement = [itemID, element];
+            event.addAdvanced(itemElement[0], (item, advance, text) => {
                 // 检查是否按下了Shift或Alt键
-                if   如果 (event   事件.isShift()) {
+                if (event.isShift()) {
                     // Shift键
-                    for (let   让 i = 1; i < itemElement.length; i++) {
-                        const   常量 element = itemElement[i];
-                        text.add(i, Text.translate   翻译(element + ".shift"));
+                    for (let i = 1; i < itemElement.length; i++) {
+                        const element = itemElement[i];
+                        text.add(i, Text.translate(element + ".shift"));
                     }
-                } else if   如果 (event   事件.isAlt()) {
+                } else if (event.isAlt()) {
                     // Alt键
-                    for (let   让 i = 1; i < itemElement.length; i++) {
-                        const   常量 element = itemElement[i];
-                        text.add(i, Text.translate   翻译(element + ".alt"));
+                    for (let i = 1; i < itemElement.length; i++) {
+                        const element = itemElement[i];
+                        text.add(i, Text.translate(element + ".alt"));
                     }
                 } else {
                     // 默认显示
-                    for (let   让 i = 1; i < itemElement.length; i++) {
-                        const   常量 element = itemElement[i];
-                        text.add(i, Text.translate   翻译(element));
+                    for (let i = 1; i < itemElement.length; i++) {
+                        const element = itemElement[i];
+                        text.add(i, Text.translate(element));
                     }
                 }
             });
