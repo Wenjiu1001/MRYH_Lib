32f5193cdc4f70bb9eefcbb21d9224bc2901f60f
Wenjiu1001 <1060967671@qq.com>
2025-05-04 19:38:09 +0800

    Update debug.js

--- server_scripts/event/debug.js
+++ server_scripts/event/debug.js
@@ -1,5 +1,15 @@
 
 
+let $BuiltInRegistries = Java.loadClass('net.minecraft.core.registries.BuiltInRegistries')
+
+$BuiltInRegistries.ENCHANTMENT.forEach((/**@type {Internal.Enchantment} key */key, value) => {
+    if (key.category == "UNIVERSAL_ENCHANTS_MRYH_KILLERADD_BULLET_3") {
+        /**@type {Internal.EnchantmentCategory} */
+        console.log(key.category.canEnchant(Item.of("tacz:modern_kinetic_gun")))
+    } else if (key.category == "UNIVERSAL_ENCHANTS_MRYH_KILLERADD_BULLET") {
+        console.log(key.category.canEnchant(Item.of("tacz:modern_kinetic_gun")))
+    }
+})
 /**
  * @param {Internal.LivingHurtEvent_} event 
  * @param {$LivingEntityHurtEventJS_} event
@@ -46,17 +56,17 @@ function handleDamageByPlayer(event) {
  */
 function BulletDamageByPlayer(event) {
     let enchantlevel = event.source.player.mainHandItem
-    // 战术射击增强
-    if (enchantlevel.getEnchantmentLevel("mryh:killeradd_bullet")) {
-        let count = event.amount * (1 + 0.1 * enchantlevel.getEnchantmentLevel("mryh:killeradd_bullet"))
+    // 增强射击战术
+    if (enchantlevel.getEnchantmentLevel("mryh:tactical_enhancement")) {
+        let count = event.amount * (1 + 0.1 * enchantlevel.getEnchantmentLevel("mryh:tactical_enhancement"))
         event.setAmount(count)
     }
     // 减速弹
-    if (enchantlevel.getEnchantmentLevel("mryh:slowness_bullet")) {
+    if (enchantlevel.getEnchantmentLevel("mryh:slowing_bullets")) {
         /**
          * @type {Internal.LivingEntity}
          */
         let entity = event.entity
-        entity.potionEffects.add('minecraft:slowness', 60, enchantlevel.getEnchantmentLevel("mryh:slowness_bullet"))
+        entity.potionEffects.add('minecraft:slowness', 60, enchantlevel.getEnchantmentLevel("mryh:slowing_bullets"))
     }
 }
