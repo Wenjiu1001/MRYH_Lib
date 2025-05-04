14ff44b3c4dfa47999ad6abc2fb15954ae0d4592
Wenjiu1001 <1060967671@qq.com>
2025-05-04 19:23:43 +0800

    Update tooltips_mryh.js

--- client_scripts/tooltips_mryh.js
+++ client_scripts/tooltips_mryh.js
@@ -1,50 +1,44 @@
 
 
-let lang
-let deflang = "kubejs/assets/mryh/lang/" + "zh_cn" + ".json";
 
-ClientEvents.loggedIn(event => {
-    //获取客户端对应lang的路径
-    lang = "kubejs/assets/mryh/lang/" + Client.languageManager.selected + ".json"
-})
-/*
-let json = JsonIO.read(lang);
-json = JsonIO.toObject(json);
-*/
-ItemEvents.tooltip(event => {
-    Object.assign(JsonIO.read(deflang), JsonIO.read(lang))
-        .forEach(element => {
-            // 根据langkey组织物品id
-            if (element.includes("item.") && element.includes("tooltip")) {
-                // 获取modid
-                let modid = element.split('.')[1];
-                // 将id和langkey组织到1个列表，以便于addAdvanced的内部函数可以访问到langkey
-                let itemID = modid + ":" + element.split('.')[2];
-                let itemElement = [itemID, element];
-                event.addAdvanced(itemElement[0], (item, advance, text) => {
-                    // 检查是否按下了Shift或Alt键
-                    if (event.isShift()) {
-                        // Shift键
-                        for (let i = 1; i < itemElement.length; i++) {
-                            const element = itemElement[i];
-                            text.add(i, Text.translate(element + ".shift"));
-                        }
-                    } else if (event.isAlt()) {
-                        // Alt键
-                        for (let i = 1; i < itemElement.length; i++) {
-                            const element = itemElement[i];
-                            text.add(i, Text.translate(element + ".alt"));
-                        }
-                    } else {
-                        // 默认显示
-                        for (let i = 1; i < itemElement.length; i++) {
-                            const element = itemElement[i];
-                            text.add(i, Text.translate(element));
-                        }
+
+ItemEvents.tooltip(event   事件 => {
+    let   让 lang = "kubejs/assets/mryh/lang/" + Client.languageManager.selected.toString() + ".json";
+    let   让 json = JsonIO.read(lang);
+    json = JsonIO.toObject(json);
+
+    json.forEach(element => {
+        // 根据langkey组织物品id
+        if   如果 (element.includes("item.") && element.includes("tooltip")) {
+            // 获取modid
+            let   让 modid = element.split   分裂('.')[1];
+            // 将id和langkey组织到1个列表，以便于addAdvanced的内部函数可以访问到langkey
+            let   让 itemID = modid + ":" + element.split   分裂('.')[2];
+            let   让 itemElement = [itemID, element];
+            event   事件.addAdvanced(itemElement[0], (item, advance, text) => {
+                // 检查是否按下了Shift或Alt键
+                if   如果 (event   事件.isShift()) {
+                    // Shift键
+                    for (let   让 i = 1; i < itemElement.length; i++) {
+                        const   常量 element = itemElement[i];
+                        text.add(i, Text.translate   翻译(element + ".shift"));
                     }
-                });
-            }
-        });
+                } else if   如果 (event   事件.isAlt()) {
+                    // Alt键
+                    for (let   让 i = 1; i < itemElement.length; i++) {
+                        const   常量 element = itemElement[i];
+                        text.add(i, Text.translate   翻译(element + ".alt"));
+                    }
+                } else {
+                    // 默认显示
+                    for (let   让 i = 1; i < itemElement.length; i++) {
+                        const   常量 element = itemElement[i];
+                        text.add(i, Text.translate   翻译(element));
+                    }
+                }
+            });
+        }
+    });
 });
 
 //例子
@@ -53,3 +47,12 @@ ItemEvents.tooltip(event => {
 "item.mryh.example.tooltip.alt": "Alt key tooltip text"
 "item.mryh.example.tooltip.shift": "Shift key tooltip text"
 */
+
+
+
+ItemEvents.tooltip(e=>{
+    e.add('zombiekit:exo_helmet',Text.yellow("辐射防护能力"))
+    e.add('zombiekit:exo_chestplate',Text.yellow("辐射防护能力"))
+    e.add('zombiekit:exo_leggings',Text.yellow("辐射防护能力"))
+    e.add('zombiekit:exo_boots',Text.yellow("辐射防护能力"))
+})
